5d50b4455f6e38c98cdf72e3607305b0e8255ce9
Felipe <fsoares@autodoc.com.br>
2023-10-10 20:32:53 -0300

    HEY-3: adding button to like

--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -2,7 +2,23 @@
 
 
 
-<div class="rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3 dark:text-gray-400">
-    {{ $question->question }}
+<div class="rounded dark:bg-gray-800/50 shadow shadow-blue-500/50 p-3 dark:text-gray-400 flex justify-between items-center">
+   <span>
+       {{ $question->question }}
+   </span>
+    <div class="flex space-x-3">
+        <x-form :action="route('question.like', $question)">
+            <button class="text-green-500">
+                <x-icons.thumbs-up class="w-5 h-6 hover:text-green-300 cursor-pointer" />
+                {{ $question->likes }}
+            </button>
+        </x-form>
+        <x-form :action="route('question.like', $question)">
+            <button class="text-red-500">
+                <x-icons.thumbs-up class="w-5 h-6 hover:text-green-300 cursor-pointer" />
+                {{ $question->unlikes }}
+            </button>
+        </x-form>
+    </div>
 </div>
 
